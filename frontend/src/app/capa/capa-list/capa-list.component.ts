860ce8ed92f8856389b999298052fe6fd9112aa1
mguaitav <miguelangelguaivi@gmail.com>
2025-10-22 15:20:26 +0200

    feat: recoger capa desde geoserver con filtros dinámicos, con datos de la base de datos
    el mapa llama al endpoint de la capa de geoserver con ciertos filtros y la plasma encima de la capa base de leaflet; el geoserver tiene la información desde la base de datos, donde se tienen datos espaciales

--- frontend/src/app/capa/capa-list/capa-list.component.ts
+++ frontend/src/app/capa/capa-list/capa-list.component.ts
@@ -11,18 +11,54 @@ import { HttpClient } from '@angular/common/http';
 export class CapaListComponent {
   map!: L.Map;
   capas?: any[];
+  private capaZonas!: L.TileLayer.WMS;
+
+  private GEOSERVER_URL = "http://185.23.121.71:1234/geoserver/vituclim/wms";
+  private LAYER_NAME = "vituclim:prueba_capa";
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.map = L.map('map').setView([40.78, -74], 10.5);
-    var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '© OpenStreetMap contributors'
-    });
-    osm.addTo(this.map);
+    this.setMap();
+    this.addBaseLayer();
+    //this.addStaticLayers();
+  }
 
-    // Consultar capas desde el backend Django
+  private setMap(): void {
+    this.map = L.map('map').setView([40.4, -3.7], 10.5);
+  }
+
+  private addBaseLayer(): void {
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { attribution: "© OpenStreetMap contributors" }).addTo(this.map);
+  }
+
+  private addGeoServerLayer(CQLFilter?: string): void {
+    if (this.capaZonas) {
+      this.map.removeLayer(this.capaZonas);
+    }
+    const params: L.WMSOptions = {
+      layers: this.LAYER_NAME,
+      format: 'image/png',
+      transparent: true,
+      version: '1.1.0',
+      attribution: 'GeoServer',
+    };
+
+    if (CQLFilter) {
+      (params as any).CQL_FILTER = CQLFilter;
+    }
+
+    this.capaZonas = L.tileLayer.wms(this.GEOSERVER_URL, params).addTo(this.map);
+    console.log(this.capaZonas);
+    
+  }
+
+  applyFilter(): void {
+    const FILTER = "tipo='urbano' AND provincia='Madrid'";
+    this.addGeoServerLayer(FILTER);
+  }
+
+  addStaticLayers(): void {
     this.http.get<any[]>('http://localhost:8000/api/capa')
       .subscribe(capas => {
         capas.forEach(capa => {
